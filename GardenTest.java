617168ac2b1eac4923a7e18f67dd7e524a64cec6
Paul Milne <hallhill@gmail.com>
2016-10-31 16:38:17 +0000

    Amended gardent test to have two plants with two names - lettuce and Daisy.

--- GardenTest.java
+++ GardenTest.java
@@ -3,10 +3,16 @@ import org.junit.*;
 
 class GardenTest{
     Garden garden;
-    Plant plant;
+    Plant plant1;
+    Plant plant2;
 
     @Before
     public void before() {
         garden = new Garden("allotment", 2);
+        plant1 = new Plant("Lettuce");
+        plant2 = new Plant("Daisy");
     }
+
+    // @Test
+
 }
