5edc0bcdd67423c9117a65380e4b370613ed9f6b
Paul Milne <hallhill@gmail.com>
2016-10-31 20:31:53 +0000

    Added two methods into the garden class file to count the plants in the veg and the flower beds.

--- Garden.java
+++ Garden.java
@@ -1,18 +1,35 @@
 public class Garden {
     private String name;
     private int size; //size refers to number of beds in the garden
-    private Plant[] veg;
-    private Plant[] flowers;
+    private Plant[] vegBed;
+    private Plant[] flowerBed;
 
     public Garden(String name, int size){
         this.name = name;
         this.size = size;
-        this.veg = new Plant[4];
-        this.flowers = new Plant[3];
+        this.vegBed = new Plant[4];
+        this.flowerBed = new Plant[3];
     }
 
     public String getName(){
         return this.name;
     }
 
+    pub int vegCount(){
+        int counter = 0;
+        for (Plant seedlings : vegBed){
+            if (seedlings != null) {
+                counter++
+            }
+        }
+    }
+
+pub int flowerCount(){
+    int counter = 0;
+    for (Plant seedlings : flowerBed){
+        if (seedlings != null) {
+            counter++
+        }
+    }
+}
 }
