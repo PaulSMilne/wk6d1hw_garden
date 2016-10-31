4864685cd0087bfdb24fbcdb5f1bef6f13b2becf
Paul Milne <hallhill@gmail.com>
2016-10-31 16:22:44 +0000

    initial commit - created 2 class files anda test file

---
+++ Garden.java
@@ -0,0 +1,16 @@
+class Garden {
+
+    private String name;
+    private int size; //size refers to number of beds in the garden
+    private Plant[] veg;
+    private Plant[] flowers;
+
+    public Garden(String name, int size){
+        this.name = name;
+        this.size = size;
+        this.veg = new Plant[4];
+        this.flowers = new Plant[3];
+
+    }
+
+}
---
+++ GardenTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+class GardenTest{
+    Garden garden;
+    Plant lettuce;
+    Plant broccoli;
+    Plant beans;
+    Plant daisy;
+    Plant marigold;
+    Plant peony;
+}
